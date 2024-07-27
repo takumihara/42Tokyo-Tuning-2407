47560d5330364000c9669a3f31d73b51ccbbdf80
Takumi Hara <takumihara1226@gmail.com>
2024-07-27 18:06:20 +0900

    Remove N+1

--- webapp/backend/src/models/order.rs
+++ webapp/backend/src/models/order.rs
@@ -15,6 +15,25 @@ pub struct Order {
 }
 
 #[derive(FromRow, Clone, Debug)]
+pub struct OrderWithData {
+    pub id: i32,
+    pub client_id: i32,
+    pub client_username: Option<String>,
+    pub dispatcher_id: Option<i32>,
+    pub dispatcher_user_id: Option<i32>,
+    pub dispatcher_username: Option<String>,
+    pub tow_truck_id: Option<i32>,
+    pub driver_user_id: Option<i32>,
+    pub driver_username: Option<String>,
+    pub status: String,
+    pub node_id: i32,
+    pub area_id: i32,
+    pub car_value: f64,
+    pub order_time: DateTime<Utc>,
+    pub completed_time: Option<DateTime<Utc>>,
+}
+
+#[derive(FromRow, Clone, Debug)]
 pub struct CompletedOrder {
     pub id: i32,
     pub order_id: i32,
