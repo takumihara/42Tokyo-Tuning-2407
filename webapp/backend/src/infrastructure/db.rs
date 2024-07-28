03144d9b3893ddd0124e3e249b5823d906ed4487
Takumi Hara <takumihara1226@gmail.com>
2024-07-28 13:21:52 +0900

    Increase max connections

--- webapp/backend/src/infrastructure/db.rs
+++ webapp/backend/src/infrastructure/db.rs
@@ -1,9 +1,12 @@
 use sqlx::mysql::MySqlPool;
+use sqlx::mysql::MySqlPoolOptions;
 use std::env;
 
 pub async fn create_pool() -> MySqlPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    MySqlPool::connect(&database_url)
+    MySqlPoolOptions::new()
+        .max_connections(10000)
+        .connect(&database_url)
         .await
         .expect("Failed to create pool")
 }
